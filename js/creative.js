279dcff910b5e079ce37e4b3fb72d81d40288b12
george <robopop@radarboy.com>
2016-04-09 16:39:54 +0800

    triangulation and bug fixes

--- js/creative.js
+++ js/creative.js
@@ -303,5 +303,68 @@ function init() {
 }
 
 
-/////////////////////////////////// fx
+ p.strokeTriangle = function(x1, y1, x2, y2, x3, y3) {
+ 	this.beginPath(); 
+     this.moveTo(x1, y1);
+     this.lineTo(x2, y2);
+     this.lineTo(x3, y3);
+     this.lineTo(x1, y1);
+     this.stroke(); 
+     this.closePath();
+}
 
+ p.fillTriangle = function(x1, y1, x2, y2, x3, y3) {
+      this.beginPath(); 
+      this.moveTo(x1, y1);
+      this.lineTo(x2, y2);
+      this.lineTo(x3, y3);
+      this.lineTo(x1, y1);
+      this.fill(); 
+      this.closePath();
+ };
+
+ 
+function triangulate(grid_w, grid_h, alpha) {
+
+	if (grid_h == undefined) {
+		grid_h = grid_w;
+	}
+
+	if (alpha == undefined) {
+		alpha = 0.8;
+	}
+
+	var ww = Math.ceil(w/grid_w);
+	var	hh = Math.ceil(h/grid_h);
+	//console.log(ww)
+    var imgData=ctx.getImageData(0,0,w,h); 
+    ctx.clearRect(0,0,w,h);
+    //var sourceBuffer8 = new Uint8Array(imgData.data.buffer);
+    //var sourceBuffer8 = new Uint8ClampedArray(imgData.data.buffer);
+    var sourceBuffer32 = new Uint32Array(imgData.data.buffer);
+    var i =0;
+    for(var x = 0; x < w; x += grid_w)
+    {
+        for(var y = 0; y < h; y += grid_h)
+        {
+
+          var pos = (x + y * w);
+          var b = (sourceBuffer32[pos] >> 16) & 0xff;
+          var g = (sourceBuffer32[pos] >> 8) & 0xff;
+          var r = (sourceBuffer32[pos] >> 0) & 0xff;
+          ctx.fillStyle = rgba(r,g,b, alpha);
+
+          if (i%2) {
+			ctx.fillTriangle(x, y - grid_h, x, y + grid_h, x - grid_w, y );
+			} else {
+			ctx.fillTriangle(x - grid_w, y - grid_h, x, y, x - grid_w , y + grid_h);
+
+			}
+
+			i++;
+        }
+    }
+
+}
+
+/////////////////////////////////// fx
